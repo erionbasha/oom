4dd389d97a14fe152994097e0cafa9e0b702fbec
erionbasha <erion_basha@yahoo.de>
2016-02-22 20:31:36 +0100

    add project Task2

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Objects
+            Vehicle v1 = new Vehicle("black", 19, 36, 50);
+            Vehicle v2 = new Vehicle("red", 19, 333, 90);
+            //Price
+            Console.WriteLine(v1.Price);
+            Console.WriteLine("" + v1.UpdatePrice(89));
+            Console.WriteLine(v1.Price);
+            //Name
+            Console.WriteLine(v1.Name);
+            v1.Name = "Harald";
+            Console.WriteLine(v1.Name);
+            v1.Name = "hello";
+            Console.WriteLine(v1.Name);
+            
+
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Vehicle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Vehicle
+    {
+        //Fields 
+        private double width, height;
+        private string m_name;
+
+        public decimal m_price;
+
+        //Constructor
+        public Vehicle( string name, double newWidth, double newHeight, decimal price)
+        {
+            if (newWidth == 20) throw new Exception("Width musst be greater then 20");
+            if (newHeight == 20) throw new Exception("Height musst be lower then 20");
+            this.width = newWidth;
+            this.height = newHeight;
+            this.m_name = name;
+            this.m_price = price;
+        }
+
+        //Properties Price
+        public decimal Price
+        {
+            get
+            {
+                return m_price;
+            }
+            set
+            {
+                if (value < 0) throw new Exception("There is no Vehicle to GIFT !!!");
+                m_price = value;
+            }
+        }
+
+        //Properties Name
+        public string Name
+        {
+            get
+            {
+                return m_name;
+            }
+            set
+            {
+                m_name = value;
+            }
+
+        }
+
+        //Methods
+        public decimal UpdatePrice( decimal newPrice )
+        {
+            if (newPrice < 0) throw new Exception("Price musst be greater then 20");
+            else
+                m_price = newPrice;
+            return m_price;
+        } 
+    }
+}
